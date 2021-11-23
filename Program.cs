ea062be84e4b52aa5ed1cfd35a4b5e59503d3414
Isaac Rovira <iroviraro@uoc.edu>
2021-11-23 23:18:51 +0100

    Eliminar la insercion de usuario del main.

--- Program.cs
+++ Program.cs
@@ -14,20 +14,7 @@ namespace NETime_WF_EF6
         [STAThread]
         static void Main()
         {
-            /*
-            var context = new netimeContainer();
-            var ususario = new user()
-            {
-                email = "email@dominiio.com",
-                name = "nombre",
-                surname = "apellidos",
-                address = "dirección",
-                phone = "tel",
-                password = "contraseña"
-            };
-            context.userSet.Add(ususario);
-            context.SaveChanges();
-            */
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
