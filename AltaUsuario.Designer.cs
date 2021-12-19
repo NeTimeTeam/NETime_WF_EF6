cb634c3a74fb9d9388e81f3b9ef251e9bdd93203
Mónica Granero Martínez <mgraneromar@uoc.edu>
2021-12-19 17:57:32 +0100

    Form AltaUsuarios

---
+++ AltaUsuario.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace NETime_WF_EF6
+{
+    partial class AltaUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AltaUsuario));
+            this.textBox_userName = new System.Windows.Forms.TextBox();
+            this.textBox_userSurname = new System.Windows.Forms.TextBox();
+            this.textBox_userEmail = new System.Windows.Forms.TextBox();
+            this.textBox_userPass = new System.Windows.Forms.TextBox();
+            this.textBox_userPhone = new System.Windows.Forms.TextBox();
+            this.textBox_userAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_userName
+            // 
+            this.textBox_userName.Location = new System.Drawing.Point(355, 159);
+            this.textBox_userName.Name = "textBox_userName";
+            this.textBox_userName.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userName.TabIndex = 0;
+            // 
+            // textBox_userSurname
+            // 
+            this.textBox_userSurname.Location = new System.Drawing.Point(355, 211);
+            this.textBox_userSurname.Name = "textBox_userSurname";
+            this.textBox_userSurname.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userSurname.TabIndex = 1;
+            // 
+            // textBox_userEmail
+            // 
+            this.textBox_userEmail.Location = new System.Drawing.Point(357, 261);
+            this.textBox_userEmail.Name = "textBox_userEmail";
+            this.textBox_userEmail.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userEmail.TabIndex = 2;
+            // 
+            // textBox_userPass
+            // 
+            this.textBox_userPass.Location = new System.Drawing.Point(357, 319);
+            this.textBox_userPass.Name = "textBox_userPass";
+            this.textBox_userPass.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userPass.TabIndex = 3;
+            // 
+            // textBox_userPhone
+            // 
+            this.textBox_userPhone.Location = new System.Drawing.Point(357, 372);
+            this.textBox_userPhone.Name = "textBox_userPhone";
+            this.textBox_userPhone.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userPhone.TabIndex = 4;
+            // 
+            // textBox_userAddress
+            // 
+            this.textBox_userAddress.Location = new System.Drawing.Point(357, 429);
+            this.textBox_userAddress.Name = "textBox_userAddress";
+            this.textBox_userAddress.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userAddress.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(235, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(235, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(200, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(235, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Teléfono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(235, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Dirección";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::NETime_WF_EF6.Properties.Resources.boton__1_;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(385, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Darme de alta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AltaUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 587);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_userAddress);
+            this.Controls.Add(this.textBox_userPhone);
+            this.Controls.Add(this.textBox_userPass);
+            this.Controls.Add(this.textBox_userEmail);
+            this.Controls.Add(this.textBox_userSurname);
+            this.Controls.Add(this.textBox_userName);
+            this.Name = "AltaUsuario";
+            this.Text = "AltaUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_userName;
+        private System.Windows.Forms.TextBox textBox_userSurname;
+        private System.Windows.Forms.TextBox textBox_userEmail;
+        private System.Windows.Forms.TextBox textBox_userPass;
+        private System.Windows.Forms.TextBox textBox_userPhone;
+        private System.Windows.Forms.TextBox textBox_userAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AltaUsuario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static NETime_WF_EF6.Form1;
+
+namespace NETime_WF_EF6
+{
+    public partial class AltaUsuario : Form
+    {
+        public AltaUsuario()
+        {
+            InitializeComponent();
+        }
+
+        //CONTEXT CLASS
+        private netimeContainer context = new netimeContainer();
+       
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Obtenemos el byte[] del password y el salt[] antes de almacenarlo en el DB.
+            PasswordHash passGen = new PasswordHash(textBox_userPass.Text);
+
+            //Creamos un objeto usuario con los datos del formulario
+            user usuario = new user()
+            {
+                name = textBox_userName.Text,
+                email = textBox_userEmail.Text,
+                password = passGen.GenerateSaltedHash(),
+                salt = passGen.Salt(),
+                surname = textBox_userSurname.Text,
+                phone = textBox_userPhone.Text,
+                //Evalua la expresión "XXX.Length > 0" y asigna uno de los dos valores definidos a continuación
+                address = textBox_userAddress.Text.Length > 0 ? textBox_userAddress.Text : "none"
+            };
+
+            int userExist = (from u in context.userSet where u.email.Equals(usuario.email) select u).Count();
+            if (userExist > 0)
+            {
+                MessageBox.Show("El usuario ya existe");
+                textBox_userEmail.ForeColor = Color.Red;
+            }
+            else
+            {
+                this.context.userSet.Add(usuario); //Le pasamos el objeto al context.
+             
+                // callback callback = clean_userTextBoxes;
+                // saveChanges("CREATE USER", callback); //Solicitamos al context que guarde los cambios en la BD.                    
+            }
+        }
+    }
+
+    }
+
