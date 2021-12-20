2ad8bc71c2153cedf9ec39963e90dd9eb2736e72
Isaac Rovira <iroviraro@uoc.edu>
2021-12-20 20:39:11 +0100

    Se creado el alta de usuarios. Se ha añadido un segundo campo para verificar el password. Se han añadido todos los eventos de los campos y se han definido sus propiedades. Se ha añadido un método asincrono de consulta a la base de datos.

--- AltaUsuario.Designer.cs
+++ AltaUsuario.Designer.cs
@@ -42,59 +42,90 @@ namespace NETime_WF_EF6
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_AddUser = new System.Windows.Forms.Button();
+            this.label_userPass2 = new System.Windows.Forms.Label();
+            this.textBox_userPass2 = new System.Windows.Forms.TextBox();
+            this.label_msg = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_userName
             // 
-            this.textBox_userName.Location = new System.Drawing.Point(355, 159);
+            this.textBox_userName.CausesValidation = false;
+            this.textBox_userName.Location = new System.Drawing.Point(40, 78);
+            this.textBox_userName.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_userName.Name = "textBox_userName";
-            this.textBox_userName.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userName.Size = new System.Drawing.Size(173, 20);
             this.textBox_userName.TabIndex = 0;
+            this.textBox_userName.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userName.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // textBox_userSurname
             // 
-            this.textBox_userSurname.Location = new System.Drawing.Point(355, 211);
+            this.textBox_userSurname.CausesValidation = false;
+            this.textBox_userSurname.Location = new System.Drawing.Point(40, 117);
+            this.textBox_userSurname.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_userSurname.Name = "textBox_userSurname";
-            this.textBox_userSurname.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userSurname.Size = new System.Drawing.Size(173, 20);
             this.textBox_userSurname.TabIndex = 1;
+            this.textBox_userSurname.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userSurname.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // textBox_userEmail
             // 
-            this.textBox_userEmail.Location = new System.Drawing.Point(357, 261);
+            this.textBox_userEmail.CausesValidation = false;
+            this.textBox_userEmail.Location = new System.Drawing.Point(40, 156);
+            this.textBox_userEmail.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_userEmail.Name = "textBox_userEmail";
-            this.textBox_userEmail.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userEmail.Size = new System.Drawing.Size(173, 20);
             this.textBox_userEmail.TabIndex = 2;
+            this.textBox_userEmail.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userEmail.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // textBox_userPass
             // 
-            this.textBox_userPass.Location = new System.Drawing.Point(357, 319);
+            this.textBox_userPass.CausesValidation = false;
+            this.textBox_userPass.Location = new System.Drawing.Point(259, 156);
+            this.textBox_userPass.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_userPass.Name = "textBox_userPass";
-            this.textBox_userPass.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userPass.PasswordChar = '*';
+            this.textBox_userPass.Size = new System.Drawing.Size(200, 20);
             this.textBox_userPass.TabIndex = 3;
+            this.textBox_userPass.UseSystemPasswordChar = true;
+            this.textBox_userPass.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userPass.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // textBox_userPhone
             // 
-            this.textBox_userPhone.Location = new System.Drawing.Point(357, 372);
+            this.textBox_userPhone.CausesValidation = false;
+            this.textBox_userPhone.Location = new System.Drawing.Point(40, 195);
+            this.textBox_userPhone.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_userPhone.Name = "textBox_userPhone";
-            this.textBox_userPhone.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userPhone.Size = new System.Drawing.Size(173, 20);
             this.textBox_userPhone.TabIndex = 4;
+            this.textBox_userPhone.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userPhone.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // textBox_userAddress
             // 
-            this.textBox_userAddress.Location = new System.Drawing.Point(357, 429);
+            this.textBox_userAddress.CausesValidation = false;
+            this.textBox_userAddress.Location = new System.Drawing.Point(259, 78);
+            this.textBox_userAddress.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_userAddress.Multiline = true;
             this.textBox_userAddress.Name = "textBox_userAddress";
-            this.textBox_userAddress.Size = new System.Drawing.Size(395, 31);
+            this.textBox_userAddress.Size = new System.Drawing.Size(200, 59);
             this.textBox_userAddress.TabIndex = 5;
+            this.textBox_userAddress.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userAddress.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(235, 159);
+            this.label1.Location = new System.Drawing.Point(37, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 6;
             this.label1.Text = "Nombre";
             // 
@@ -103,9 +134,10 @@ namespace NETime_WF_EF6
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(235, 211);
+            this.label2.Location = new System.Drawing.Point(37, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.Size = new System.Drawing.Size(57, 15);
             this.label2.TabIndex = 7;
             this.label2.Text = "Apellidos";
             // 
@@ -114,9 +146,10 @@ namespace NETime_WF_EF6
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(235, 261);
+            this.label3.Location = new System.Drawing.Point(37, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 29);
+            this.label3.Size = new System.Drawing.Size(39, 15);
             this.label3.TabIndex = 8;
             this.label3.Text = "Email";
             // 
@@ -125,9 +158,10 @@ namespace NETime_WF_EF6
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(200, 319);
+            this.label4.Location = new System.Drawing.Point(256, 139);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 29);
+            this.label4.Size = new System.Drawing.Size(70, 15);
             this.label4.TabIndex = 9;
             this.label4.Text = "Contraseña";
             // 
@@ -136,9 +170,10 @@ namespace NETime_WF_EF6
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(235, 374);
+            this.label5.Location = new System.Drawing.Point(37, 178);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(110, 29);
+            this.label5.Size = new System.Drawing.Size(55, 15);
             this.label5.TabIndex = 10;
             this.label5.Text = "Teléfono";
             // 
@@ -147,32 +182,73 @@ namespace NETime_WF_EF6
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(235, 429);
+            this.label6.Location = new System.Drawing.Point(256, 61);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(115, 29);
+            this.label6.Size = new System.Drawing.Size(59, 15);
             this.label6.TabIndex = 11;
             this.label6.Text = "Dirección";
             // 
-            // button1
+            // button_AddUser
             // 
-            this.button1.BackgroundImage = global::NETime_WF_EF6.Properties.Resources.boton__1_;
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(385, 491);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(305, 46);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Darme de alta";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_AddUser.BackgroundImage = global::NETime_WF_EF6.Properties.Resources.boton__1_;
+            this.button_AddUser.Enabled = false;
+            this.button_AddUser.ForeColor = System.Drawing.SystemColors.Control;
+            this.button_AddUser.Location = new System.Drawing.Point(163, 308);
+            this.button_AddUser.Margin = new System.Windows.Forms.Padding(2);
+            this.button_AddUser.Name = "button_AddUser";
+            this.button_AddUser.Size = new System.Drawing.Size(152, 24);
+            this.button_AddUser.TabIndex = 12;
+            this.button_AddUser.Text = "Darme de alta";
+            this.button_AddUser.UseVisualStyleBackColor = true;
+            this.button_AddUser.Click += new System.EventHandler(this.button_AddUser_Click);
+            // 
+            // label_userPass2
+            // 
+            this.label_userPass2.AutoSize = true;
+            this.label_userPass2.BackColor = System.Drawing.Color.Transparent;
+            this.label_userPass2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_userPass2.Location = new System.Drawing.Point(256, 178);
+            this.label_userPass2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_userPass2.Name = "label_userPass2";
+            this.label_userPass2.Size = new System.Drawing.Size(111, 15);
+            this.label_userPass2.TabIndex = 14;
+            this.label_userPass2.Text = "Repetir contraseña";
+            // 
+            // textBox_userPass2
+            // 
+            this.textBox_userPass2.CausesValidation = false;
+            this.textBox_userPass2.Location = new System.Drawing.Point(259, 195);
+            this.textBox_userPass2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_userPass2.Name = "textBox_userPass2";
+            this.textBox_userPass2.PasswordChar = '*';
+            this.textBox_userPass2.Size = new System.Drawing.Size(200, 20);
+            this.textBox_userPass2.TabIndex = 13;
+            this.textBox_userPass2.UseSystemPasswordChar = true;
+            this.textBox_userPass2.CausesValidationChanged += new System.EventHandler(this.textBox_CausesValidationChanged);
+            this.textBox_userPass2.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label_msg
+            // 
+            this.label_msg.AutoSize = true;
+            this.label_msg.Location = new System.Drawing.Point(37, 256);
+            this.label_msg.Name = "label_msg";
+            this.label_msg.Size = new System.Drawing.Size(55, 13);
+            this.label_msg.TabIndex = 15;
+            this.label_msg.Text = "Response";
+            this.label_msg.Visible = false;
             // 
             // AltaUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(982, 587);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(491, 368);
+            this.Controls.Add(this.label_msg);
+            this.Controls.Add(this.label_userPass2);
+            this.Controls.Add(this.textBox_userPass2);
+            this.Controls.Add(this.button_AddUser);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -185,6 +261,7 @@ namespace NETime_WF_EF6
             this.Controls.Add(this.textBox_userEmail);
             this.Controls.Add(this.textBox_userSurname);
             this.Controls.Add(this.textBox_userName);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AltaUsuario";
             this.Text = "AltaUsuario";
             this.ResumeLayout(false);
@@ -206,6 +283,9 @@ namespace NETime_WF_EF6
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_AddUser;
+        private System.Windows.Forms.Label label_userPass2;
+        private System.Windows.Forms.TextBox textBox_userPass2;
+        private System.Windows.Forms.Label label_msg;
     }
 }
--- AltaUsuario.cs
+++ AltaUsuario.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,127 @@ namespace NETime_WF_EF6
 
         //CONTEXT CLASS
         private netimeContainer context = new netimeContainer();
-       
-      
-        private void button1_Click(object sender, EventArgs e)
+        private void saveChanges(string fnDesc, res callback)
+        {   
+            responseMsg("Resgistrando...", Color.Black);            
+            if (saveChanges(this.context, fnDesc).Result)
+            {
+                this.Close();
+            }
+            else
+            {
+                callback("Error de acceso a la base de datos.", Color.Red);
+            }
+        }
+        private void saveChanges(string fnDesc)
+        {
+            saveChanges(this.context, fnDesc);
+        }
+        private async Task<bool> saveChanges(netimeContainer context, string fnDesc)
+        {
+            try
+            {
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            catch (DBConcurrencyException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            catch (DbEntityValidationException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            catch (NotSupportedException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            catch (ObjectDisposedException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            catch (InvalidOperationException err)
+            {
+                MessageBox.Show(err.Message, fnDesc);
+            }
+            return false;
+        }        
+
+        //DELEGATES
+        public delegate void callback();
+        public delegate void res(string msg, Color color);
+
+        //VERIFIACIÓN DE LOS DATOS INTRODUCIDOS
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            isValidTextBox(textBox);
+        }
+        private void isValidTextBox(TextBox textBox)
+        {
+            switch (textBox.Name)
+            {
+                case "textBox_userEmail":
+                    setTextBoxStatus(Utilites.emailValidation(textBox.Text), textBox);
+                    break;
+                case "textBox_userName":
+                case "textBox_userSurname":
+                    setTextBoxStatus(Utilites.nameValidation(textBox.Text), textBox);
+                    break;
+                case "textBox_userPhone":
+                    setTextBoxStatus(Utilites.phoneValidation(textBox.Text), textBox);
+                    break;
+                case "textBox_userAddress":
+                    setTextBoxStatus(Utilites.descriptionValidation(textBox.Text), textBox);
+                    break;
+                case "textBox_userPass":
+                case "textBox_userPass2":
+                    TextBox[] textBoxes = { textBox_userPass, textBox_userPass2 };
+                    bool valid = Utilites.passwordValidation(textBox.Text) && textBox_userPass.Text.Equals(textBox_userPass2.Text);
+                    setTextBoxStatus(valid, textBoxes);
+                    break;
+            }
+        }
+        private void setTextBoxStatus(bool valid, TextBox textbox)
+        {
+            Color color = valid ? Color.Black : Color.Red;
+            textbox.ForeColor = color;
+            textbox.CausesValidation = valid;
+        }
+        private void setTextBoxStatus(bool valid, TextBox[] textBoxes)
+        {
+            foreach(TextBox tb in textBoxes)
+            {
+                setTextBoxStatus(valid, tb);
+            }
+        }
+        private void textBox_CausesValidationChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox;
+            switch (textBox.Name)
+            {
+                case "textBox_userName":
+                case "textBox_userEmail":
+                case "textBox_userSurname":
+                case "textBox_userPass":
+                case "textBox_userAddress":
+                case "textBox_userPhone":
+                    checkUserTextboxStatus();
+                    break;
+            }
+        }               
+        private void checkUserTextboxStatus()
+        {
+            button_AddUser.Enabled = (textBox_userAddress.CausesValidation & textBox_userEmail.CausesValidation & textBox_userName.CausesValidation & textBox_userPass.CausesValidation &
+                    textBox_userPhone.CausesValidation & textBox_userSurname.CausesValidation);
+        }
+
+        //ACCiÖN DEL BOTÓN AÑADIR USUARIO.
+        private void button_AddUser_Click(object sender, EventArgs e)
         {
             //Obtenemos el byte[] del password y el salt[] antes de almacenarlo en el DB.
             PasswordHash passGen = new PasswordHash(textBox_userPass.Text);
@@ -43,18 +163,24 @@ namespace NETime_WF_EF6
             int userExist = (from u in context.userSet where u.email.Equals(usuario.email) select u).Count();
             if (userExist > 0)
             {
-                MessageBox.Show("El usuario ya existe");
-                textBox_userEmail.ForeColor = Color.Red;
+                //MessageBox.Show("El usuario ya existe");
+                responseMsg($"Error: El email {usuario.email} ya existe en la base de datos.", Color.Red);
+                setTextBoxStatus(false, textBox_userEmail);
             }
             else
             {
                 this.context.userSet.Add(usuario); //Le pasamos el objeto al context.
-             
-                // callback callback = clean_userTextBoxes;
-                // saveChanges("CREATE USER", callback); //Solicitamos al context que guarde los cambios en la BD.                    
+                saveChanges("CREATE USER", responseMsg); //Solicitamos al context que guarde los cambios en la BD.
             }
+        }
+
+        //RESPONSE
+        private void responseMsg(string msg, Color color)
+        {
+            label_msg.Text = msg;
+            label_msg.ForeColor = color;
+            label_msg.Visible = true;
         }
     }
 
-    }
-
+}
